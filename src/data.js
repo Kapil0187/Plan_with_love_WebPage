06b1162462d937b17716f350dfb085686f6c2b64
Kapil0187 <kapilbarsker2003@gmail.com>
2023-12-07 03:06:38 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,53 @@
+const data = [
+    {
+        id : 1,
+        name : "Agra",
+        info : "Agra: A picturesque town nestled amidst hills. Cobblestone streets weave past historic buildings, cafes, and markets in a vibrant square. Rich in history, with a medieval castle and cultural treasures. Nature trails offer serene escapes. Warm locals embody timeless charm and hospitality.",
+        image : "https://images.unsplash.com/photo-1564507592333-c60657eea523?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YWdyYXxlbnwwfHwwfHx8MA%3D%3D",
+        price : "₹35,750",
+    },
+    {
+        id : 2,
+        name : "Jaipur",
+        info : "Jaipur, the 'Pink City,' boasts majestic palaces like Hawa Mahal & Amer Fort, showcasing Rajputana grandeur. Colorful bazaars, vibrant culture, and delectable cuisine adorn its bustling streets. Rich history, intricate architecture, and warm hospitality define this city's royal essence. Jaipur enchants with its regal charm, traditional artistry, and enduring heritage.",
+        image : "https://images.unsplash.com/photo-1603262110263-fb0112e7cc33?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8SmFpcHVyfGVufDB8fDB8fHww",
+        price : "₹38,750",
+    },
+    {
+        id : 3,
+        name : "Goa",
+        info : "Goa, India's coastal gem, beckons with golden beaches, palm trees, and vibrant culture. Rich in Portuguese heritage, it offers lively markets, historic churches, and a tapestry of flavors in its cuisine. A haven for beach lovers and party enthusiasts, Goa's blend of relaxation, adventure, and nightlife creates an irresistible charm.",
+        image : "https://images.unsplash.com/photo-1512343879784-a960bf40e7f2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8R29hfGVufDB8fDB8fHww",
+        price : "₹39,740",
+    },
+    {
+        id : 4,
+        name : "Varanasi",
+        info : " Varanasi, the spiritual heart of India, charms with ancient temples, ghats along the sacred Ganges, and bustling narrow lanes. A city steeped in tradition, it exudes spirituality, hosting mesmerizing Ganga Aarti ceremonies. Varanasi's rich cultural tapestry, vibrant bazaars, and labyrinthine alleys evoke a timeless allure, drawing pilgrims and seekers from around the world to experience its mystique and profound spirituality.",
+        image : "https://images.unsplash.com/photo-1646578515903-67873a5398f9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dmFyYW5zaXxlbnwwfHwwfHx8MA%3D%3D",
+        price : "₹40,750",
+    },
+    {
+        id : 5,
+        name : "Darjeeling",
+        info : "Darjeeling, nestled in the Himalayas, captivates with its lush tea gardens, panoramic vistas, and the iconic toy train. Its colonial charm, serene monasteries, and breathtaking landscapes beckon nature enthusiasts to explore this mountain haven.",
+        image : "https://images.unsplash.com/photo-1622308644420-b20142dc993c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8RGFyamVlbGluZ3xlbnwwfHwwfHx8MA%3D%3D",
+        price : "₹50,750",
+    },
+    {
+        id : 6,
+        name : "Jaisalmer",
+        info : "Jaisalmer, the 'Golden City' of India, mesmerizes with its sandcastle-like forts, intricate Havelis, and vast Thar Desert. Its rich Rajasthani heritage, camel safaris, and vibrant bazaars offer a glimpse into ancient trading routes. Jaisalmer's golden hues, cultural splendor, and desert charm create an enchanting experience amidst the dunes.",
+        image : "https://images.unsplash.com/photo-1600954700722-b9a768fc9397?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8SmFpc2FsbWVyfGVufDB8fDB8fHww",
+        price : "₹29,750",
+    },
+    {
+        id : 7,
+        name : "Kochi",
+        info : "Kochi, Kerala's coastal jewel, enthralls with a blend of history, culture, and scenic beauty. Its bustling port, Chinese fishing nets, and colonial architecture mirror its diverse heritage. The city's spice markets, serene backwaters, and Kathakali performances showcase its vibrant soul. Kochi's unique charm, fusion of traditions, and tranquil ambiance make it a captivating destination for travelers seeking a glimpse of Kerala's allure.",
+        image : "https://images.unsplash.com/photo-1605955794720-651b9ae7f5e7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8S29jaGl8ZW58MHx8MHx8fDA%3D",
+        price : "₹43,750",
+    },
+]
+
+export default data;
